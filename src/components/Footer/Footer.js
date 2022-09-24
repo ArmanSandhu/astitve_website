eead242ff791683b5d2b50ee21a364efd2ccd26f
Arman Sandhu <armanssandhu@gmail.com>
2022-09-23 20:52:08 -0700

    Mobile Footer Fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <div className="footer">
             <div className='footer_inner'>
-                <div> All images belong to Astitive or ar Creative Commons licensed and author attributed.</div>
+                <div> All images belong to Astitive or are Creative Commons licensed and author attributed.</div>
                 <div>&copy; 2022 | Astitve</div>
             </div>
         </div>
